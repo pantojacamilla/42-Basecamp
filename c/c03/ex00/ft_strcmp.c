0d4a31a886bfd4f107829282daa281457da90a60
clobato- <clobato-@student.42sp.org.br>
2021-04-15 23:36:20 +0000

    Corrigi a 00 e 01

--- c/c03/ex00/ft_strcmp.c
+++ c/c03/ex00/ft_strcmp.c
@@ -6,11 +6,11 @@
 /*   By: clobato- <clobato-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 12:50:52 by clobato-          #+#    #+#             */
-/*   Updated: 2021/04/14 22:38:13 by clobato-         ###   ########.fr       */
+/*   Updated: 2021/04/15 23:23:29 by clobato-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
 	int				i;
 	unsigned char	*s1_casted;
@@ -19,17 +19,13 @@ int		ft_strcmp(char *s1, char *s2)
 	i = 0;
 	s1_casted = (unsigned char *)s1;
 	s2_casted = (unsigned char *)s2;
-	while ((s1[i] != '\0' && s2[i] != '\0') || (s1[i] != s2[i]))
+	while (s1[i] != '\0')
 	{
-		if (s1[i] > s2[i])
-		{
-			return ((s2_casted[i] - s1_casted[i]));
-		}
-		else if (s1[i] < s2[i])
+		if (s1[i] != s2[i])
 		{
 			return (s1_casted[i] - s2_casted[i]);
 		}
 		i++;
 	}
-	return (0);
+	return (s1_casted[i] - s2_casted[i]);
 }
--- c/c03/ex01/ft_strncmp.c
+++ c/c03/ex01/ft_strncmp.c
@@ -6,39 +6,22 @@
 /*   By: clobato- <clobato-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 12:50:52 by clobato-          #+#    #+#             */
-/*   Updated: 2021/04/15 00:00:24 by clobato-         ###   ########.fr       */
+/*   Updated: 2021/04/15 23:29:46 by clobato-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-_Bool	ft_is_it_correct(char s1, char s2, unsigned int n, unsigned int i)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	if (((s1 != '\0' && s2 != '\0') || (s1 != s2)) && (i < (n - 1)))
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int		ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
-	unsigned char	*s1_casted;
-	unsigned char	*s2_casted;
+	unsigned int i;
 
 	i = 0;
-	s1_casted = (unsigned char *)s1;
-	s2_casted = (unsigned char *)s2;
-	while (ft_is_it_correct(s1[i], s2[i], n, i) == 1)
+	if (n == 0)
 	{
-		if (s1[i] > s2[i])
-		{
-			return ((s2_casted[i] - s1_casted[i]));
-		}
-		if (s1[i] < s2[i])
-		{
-			return (s1_casted[i] - s2_casted[i]);
-		}
+		return (0);
+	}
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+	{
 		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
